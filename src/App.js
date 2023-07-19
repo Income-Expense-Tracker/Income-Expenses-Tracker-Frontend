001dac1fa90eb083f921b7dfa84e055bf5a27104
Anmol Soni <anmolindia.soni@gmail.com>
2023-07-19 18:04:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Forms/Login";
+import HomePage from "./components/HomePage/HomePage";
+import Navbar from "./components/Navbar/Navbar";
+import Register from "./components/Forms/Register";
+import AccountDashboard from "./components/Dashbaord/AccountDashboard";
+import AddTransaction from "./components/Forms/AddTransaction";
+import AccountDetails from "./components/Dashbaord/AccountDetails";
+import AddAccount from "./components/Forms/AddAccount";
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/add-transaction/:id" element={<AddTransaction />} />
+        <Route path="/dashboard" element={<AccountDashboard />} />
+        <Route path="/dashboard/accounts/create" element={<AddAccount />} />
+        <Route
+          path="/account-details/:accountID"
+          element={<AccountDetails />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/utils/apiURL.js
@@ -0,0 +1,5 @@
+export const API_URL_USER = "http://localhost:9070/api/v1/users";
+
+export const API_URL_TRANSACTION = "http://localhost:9070/api/v1/transactions";
+
+export const API_URL_ACC = "http://localhost:9070/api/v1/accounts";
